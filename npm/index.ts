9d857dc465bd51bf83ac5d6a24ec584543610727
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-07-23 16:50:28 +0100

    feat: add npm package for all tsconfig templates

---
+++ npm/index.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs';
+import { join } from 'path';
+const baseDir = join(__dirname, 'bases');
+
+// List all tsconfig files
+const getTsconfigFiles = () => fs.readdirSync('./bases').filter((file: any) => file.match(/.*\.json$/));
+
+// get tsconfigs names
+const getTsConfigNames = () => getTsconfigFiles().map((file: any) => file.replace(".json", "").replace(".combined", ""));
+
+// get tsconfigs filename and display title
+const getTsConfigs = () => getTsconfigFiles().map((file: any) => ({filename: file, name: file.replace(".json", "").replace(".combined", "")}));
+
+// parse tsconfig file content
+const parseTsConfigFile = (tsconfigFile: string) => JSON.parse(fs.readFileSync(join(baseDir, tsconfigFile), 'utf8'));
+
+export {
+    getTsconfigFiles,
+    getTsConfigNames,
+    getTsConfigs,
+    parseTsConfigFile
+}
