65b4a3c952d8ceef4b972bba6367bcc543837fd1
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-07-23 20:53:09 +0100

    fix: use baseDir

--- npm/index.ts
+++ npm/index.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 const baseDir = join(__dirname, 'bases');
 
 // List all tsconfig files
-const getTsConfigFiles = () => fs.readdirSync('./bases').filter((file: any) => file.match(/.*\.json$/));
+const getTsConfigFiles = () => fs.readdirSync(baseDir).filter((file: any) => file.match(/.*\.json$/));
 
 // get tsconfigs names
 const getTsConfigNames = () => getTsConfigFiles().map((file: any) => file.replace(".json", "").replace(".combined", ""));
@@ -15,7 +15,10 @@ const getTsConfigs = () => getTsConfigFiles().map((file: any) => ({filename: fil
 const parseTsConfigFile = (tsconfigFile: string) => JSON.parse(fs.readFileSync(join(baseDir, tsconfigFile), 'utf8'));
 
 const copyTsConfigFile = (tsconfigFile: string, dest= process.cwd(), filename = 'tsconfig.json') => {
-    const tsconfigFilePath = join(baseDir, tsconfigFile);
+    let tsconfigFilePath = join(baseDir, tsconfigFile);
+    if(!fs.existsSync(tsconfigFilePath)) {
+        tsconfigFilePath = join(baseDir, 'recommended.json');
+    }
     fs.copyFileSync(tsconfigFilePath, join(dest, filename));
 }
 
